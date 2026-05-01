811ca6700da77df85637d2e08193b04fc66b4943
robbytrujillo <robbyikusuma88@gmail.com>
2026-05-02 06:55:53 +0700

    values app

--- admin/template.php
+++ admin/template.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
 
+    <link rel="icon" type="image/png" href="../assets/images/logo-sma.png">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
--- guru/template.php
+++ guru/template.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel Guru</title>
+    <title>Guru Panel</title>
 
     <link rel="icon" type="image/png" href="../assets/images/logo-sma.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
